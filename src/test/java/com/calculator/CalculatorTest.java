3f225441806dd361009c4ba671f3a7b26f79a1b1
DevOps Engineer <devops@example.com>
2026-04-30 11:13:50 +0500

    msg

--- src/test/java/com/calculator/CalculatorTest.java
+++ src/test/java/com/calculator/CalculatorTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
     
     private Calculator calculator;
-    //Updating for Testing purpose
+    //Updating
     
     @Before
     public void setUp() {
